d47b6b2d58bd948950fc5f8c28fb35a4822b2f56
Tomas Salgado <tomassalgadodev@gmail.com>
2023-03-10 15:19:53 -0700

    component: ArtistContainer

---
+++ src/ArtistContainer/ArtistContainer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './ArtistContainer.css';
+
+const ArtistContainer = ({ artistData }) => {
+
+    const artistCards = artistData
+
+    return (
+        <div>
+            <h2>Artist Container</h2>
+        </div>
+    )
+}
+
+export default ArtistContainer;
--- src/SearchPage/SearchPage.js
+++ src/SearchPage/SearchPage.js
@@ -1,11 +1,16 @@
 import React from "react";
 import './SearchPage.css';
 
+import artistData from "../artistData";
+
+import ArtistContainer from "../ArtistContainer/ArtistContainer";
+
 const SearchPage = ({ searchTerm}) => {
     return (
         <React.Fragment>
             <h1>-- This is where the Search Page will go --</h1>
             <p>Search Term: {searchTerm}</p>
+            <ArtistContainer artistData={artistData} />
         </React.Fragment>
     )
 }
