ff038659a51ae2d98df1d926c4a5d1e13d352fad
Tomas Salgado <tomassalgadodev@gmail.com>
2023-03-11 11:57:57 -0700

    component: AlbumContainer

---
+++ src/AlbumContainer/AlbumContainer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './AlbumContainer.css'
+
+const AlbumContainer = () => {
+    return (
+        <h3>-- Albums go here --</h3>
+    )
+}
+
+export default AlbumContainer;
--- src/ArtistPage/ArtistPage.js
+++ src/ArtistPage/ArtistPage.js
@@ -2,11 +2,14 @@ import React from "react";
 import './ArtistPage.css';
 import albumData from "../albumData";
 
+import AlbumContainer from "../AlbumContainer/AlbumContainer";
+
 const ArtistPage = ({ artistID }) => {
     return (
         <div>
             <h2>-- This is where the Artist page will go --</h2>
             <p>Artist ID: {artistID}</p>
+            <AlbumContainer />
         </div>
     )
 }
