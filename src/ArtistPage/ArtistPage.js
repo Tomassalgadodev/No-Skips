4453bc4fbd3adf00f19d891c971e229b0799ecc2
Tomas Salgado <tomassalgadodev@gmail.com>
2023-03-12 13:40:35 -0600

    feat: Display artist info on artist page via fetch from server

--- src/ArtistPage/ArtistPage.js
+++ src/ArtistPage/ArtistPage.js
@@ -1,25 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './ArtistPage.css';
 import singleArtistData from "../singleArtistData";
 
 import AlbumContainer from "../AlbumContainer/AlbumContainer";
 
-const artistImage = singleArtistData.artistImage.substring(23, singleArtistData.artistImage.length - 3);
+let artistImage;
 
 const ArtistPage = ({ artistID }) => {
-    return (
-        <div>
-            <div 
-                className="artist-heading"
-                style={{backgroundImage: `url(${artistImage})`}}
-            >
-                <h2>{singleArtistData.artistName}</h2>
-                <p>Artist ID: {artistID}</p>
-                {/* <img className="artist-image" src={artistImage}/> */}
+
+    const [loading, setLoading] = useState(true);
+    const [artistData, setArtistData] = useState({});
+
+    useEffect(() => {
+        setLoading(true);
+
+        fetch(`http://localhost:8000/api/v1/artist/${artistID}`)
+            .then(res => res.json())
+            .then(data => {
+                setArtistData(data);
+                setLoading(false);
+                artistImage = data.artistInfo.artistImage.substring(23, singleArtistData.artistImage.length - 3);
+            })
+
+    }, [artistID]);
+
+    if (!loading) {
+        return (
+            <div>
+                <div 
+                    className="artist-heading"
+                    style={{backgroundImage: `url(${artistImage})`}}
+                >
+                    <h2>{artistData.artistInfo.artistName}</h2>
+                    {/* <img className="artist-image" src={artistImage}/> */}
+                </div>
+                <AlbumContainer albumData={artistData.artistInfo.albums} />
             </div>
-            <AlbumContainer albumData={singleArtistData.albums} />
-        </div>
-    )
+        )
+    } else {
+        return (
+            <h2>-- LOADING --</h2>
+        )
+    }
+
+
 }
 
 export default ArtistPage;
--- src/SearchPage/SearchPage.js
+++ src/SearchPage/SearchPage.js
@@ -1,23 +1,25 @@
 import React, { useEffect, useState } from "react";
 import './SearchPage.css';
 
-import artistData from "../artistData";
-
 import ArtistContainer from "../ArtistContainer/ArtistContainer";
 
 const SearchPage = ({ searchTerm }) => {
 
     const [loading, setLoading] = useState(true);
-    const [searchResults, setSearchResults] = useState([]);
+    const [searchResults, setSearchResults] = useState({});
 
     useEffect(() => {
+
+        setLoading(true);
+
         fetch(`http://localhost:8000/api/v1/artistSearch/${searchTerm}`)
             .then(res => res.json())
             .then(data => {
                 setSearchResults(data);
                 setLoading(false);
             });
-    }, []);
+            
+    }, [searchTerm]);
 
 
     if (!loading) {
