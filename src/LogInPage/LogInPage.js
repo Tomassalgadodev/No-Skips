1b55004a276f66e7547d3a0fc9380b6edb39b67c
Tomas Salgado <tomassalgadodev@gmail.com>
2023-03-17 13:13:55 -0600

    feat: Write login function in LogInPage component

--- src/LogInPage/LogInPage.js
+++ src/LogInPage/LogInPage.js
@@ -6,8 +6,37 @@ const LogInPage = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
+    const login = async e => {
+
+        e.preventDefault();
+
+        try {
+
+            const loginInfo = { username, password };
+
+            const attempt = await fetch('http://localhost:8000/api/v1/login', {
+                method: 'POST',
+                body: JSON.stringify(loginInfo),
+                headers: {
+                    "Content-Type": "application/JSON"
+                }
+            });
+    
+            if (!attempt.ok) {
+                throw new Error ('Login failed');
+            } 
+
+            const login = await attempt.json();
+            
+            console.log(login);
+
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
     return (
-        <form className="log-in-form">
+        <form className="log-in-form" onSubmit={login}>
             <h2>Welcome back!</h2>
             <input placeholder="Username" value={username} onChange={e => setUsername(e.target.value)} required/>
             <input placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} required/>
