5b68b78f4a379515327adc4f38f496e758c84eb5
Tomas Salgado <tomassalgadodev@gmail.com>
2023-03-17 12:25:36 -0600

    feat: Make login form a controlled form

--- src/LogInPage/LogInPage.js
+++ src/LogInPage/LogInPage.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import './LogInPage.css';
 
 const LogInPage = () => {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
         <form className="log-in-form">
             <h2>Welcome back!</h2>
-            <input placeholder="Username" required/>
-            <input placeholder="Password" required/>
+            <input placeholder="Username" value={username} onChange={e => setUsername(e.target.value)} required/>
+            <input placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} required/>
             <button>Log In</button>
         </form>
     )
