690231face4151ee70bdf681652e351e576fb537
Tomas Salgado <tomassalgadodev@gmail.com>
2023-03-18 13:05:55 -0600

    feat: Make sign up form controlled and create functionality to ensure that the two password fields match

--- src/SignUpPage/SignUpPage.js
+++ src/SignUpPage/SignUpPage.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { Redirect } from "react-router-dom";
 import './SignUpPage.css';
 
 const SignUpPage = ({ loggedIn }) => {
+
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [passwordConfirm, setPasswordConfirm] = useState('');
+    const [passwordMatch, setPasswordMatch] = useState(false);
+
 
     if (loggedIn) {
         return (
@@ -10,16 +19,36 @@ const SignUpPage = ({ loggedIn }) => {
         )
     }
 
+    const confirmPasswordMatch = e => {
+        if (e.target.name === 'password') {
+            setPassword(e.target.value);
+            setPasswordMatch(e.target.value === passwordConfirm);
+        } else {
+            setPasswordConfirm(e.target.value);
+            setPasswordMatch(e.target.value === password);
+        }
+    }
+
+    const createNewUser = e => {
+        e.preventDefault();
+
+        if (!passwordMatch) {
+            // Show the user a message that they must match in the form
+            console.log('Passwords must match');
+        } else {
+            console.log('They match!')
+        }
+    }
+
     return (
-        <form className="sign-up-form">
+        <form onSubmit={createNewUser} className="sign-up-form">
             <h2>Create an account</h2>
-            <input placeholder="First Name" required/>
-            <input placeholder="Last Name" required/>
-            <input placeholder="Email" required/>
-            <input placeholder="Date of Birth" required/>
-            <input placeholder="Username" required/>
-            <input placeholder="Password" required/>
-            <input placeholder="Password" required/>
+            <input placeholder="First Name" value={firstName} onChange={e => setFirstName(e.target.value)} required/>
+            <input placeholder="Last Name" value={lastName} onChange={e => setLastName(e.target.value)} required/>
+            <input placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} required/>
+            <input placeholder="Username" value={username} onChange={e => setUsername(e.target.value)} required/>
+            <input placeholder="Password" value={password} name='password' onChange={confirmPasswordMatch} required/>
+            <input placeholder="Password" value={passwordConfirm} onChange={confirmPasswordMatch} required/>
             <button>Create Account</button>
         </form>
     )
