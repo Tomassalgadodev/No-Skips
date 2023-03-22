e351e15487a3d9adf53f4ef858cd00f17cc27a4d
Tomas Salgado <tomassalgadodev@gmail.com>
2023-03-22 13:38:53 -0600

    feat: Saved albums are now show from most recently liked to oldest

--- src/SavedAlbumContainer/SavedAlbumContainer.js
+++ src/SavedAlbumContainer/SavedAlbumContainer.js
@@ -5,7 +5,7 @@ import SavedAlbumCard from "../SavedAlbumCard/SavedAlbumCard";
 
 const SavedAlbumContainer = ({ savedAlbums, removeAlbum }) => {
 
-    const savedAlbumCards = savedAlbums.map(album => {
+    const savedAlbumCards = savedAlbums.reverse().map(album => {
         return (
             <SavedAlbumCard 
                 albumArt={album.albumArt}
