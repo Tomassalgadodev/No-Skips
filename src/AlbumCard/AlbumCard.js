e2995b903121bb0d5e1500ad2d41c4ac7fdf8b36
Tomas Salgado <tomassalgadodev@gmail.com>
2023-03-24 14:49:36 -0600

    style: On artist page show heart icon only when mouse is focused on album card and the album is unliked. When focusing mouse on the heart icon of an unliked album highlight it

--- src/AlbumCard/AlbumCard.js
+++ src/AlbumCard/AlbumCard.js
@@ -1,24 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import './AlbumCard.css';
 
 import likedIcon from '../assets/liked_icon.png';
 import unlikedIcon from '../assets/unliked_icon.png';
+import whiteUnlikedIcon from '../assets/unliked_white_icon.png';
 
 const AlbumCard = ({ albumArt, albumTitle, yearReleased, link, isLiked, artistName, artistID, saveAlbum, removeAlbum }) => {
 
+    const [unlikedHeart, setUnlikedHeart] = useState(unlikedIcon);
+    const [unlikedVisibility, setUnlikedVisbility] = useState('heart-icon hidden');
+
     const albumData = { albumArt, albumTitle, yearReleased, link, artistName, artistID };
 
+    const highlightHeartIcon = () => {
+        if (!isLiked) {
+            setUnlikedHeart(whiteUnlikedIcon);
+        }
+    }
+
+    const unhighlightHeartIcon = () => {
+        if (!isLiked) {
+            setUnlikedHeart(unlikedIcon);
+        }
+    }
+
+    const showHeartIcon = () => {
+        if (!isLiked) {
+            setUnlikedVisbility('heart-icon');
+        }
+    }
+
+    const hideHeartIcon = () => {
+        if (!isLiked) {
+            setUnlikedVisbility('heart-icon hidden');
+        }
+    }
+
     return (
-        <div className="album-card" id={link}>
+        <div className="album-card" 
+            id={link}
+            onMouseOver={showHeartIcon}
+            onMouseOut={hideHeartIcon}
+        >
             <img className="album-art" src={albumArt} />
             <div className="title-container">
                 <div className="title-box">
                     <h2 className="album-title">{albumTitle}</h2>
                 </div>
                 <img 
-                    className="heart-icon" 
-                    src={isLiked ? likedIcon : unlikedIcon} 
+                    className={isLiked ? 'heart-icon' : unlikedVisibility} 
+                    src={isLiked ? likedIcon : unlikedHeart} 
                     onClick={() => isLiked ? removeAlbum({ link }) : saveAlbum(albumData)}
+                    onMouseOver={highlightHeartIcon}
+                    onMouseOut={unhighlightHeartIcon}
                 />
             </div>
             <div className="details-container">
