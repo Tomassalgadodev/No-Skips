78217ae7d5c05e127c598c4069bcfb757a5aaab2
Tomas Salgado <tomassalgadodev@gmail.com>
2023-03-27 14:05:30 -0600

    style: Log in error message

--- src/LogInPage/LogInPage.js
+++ src/LogInPage/LogInPage.js
@@ -3,6 +3,7 @@ import { Redirect, useHistory, NavLink } from "react-router-dom";
 import './LogInPage.css';
 
 import logo from '../assets/anthology-logo.png';
+import errorIcon from '../assets/error-icon.png';
 
 const LogInPage = ({ loginUser, loggedIn }) => {
 
@@ -47,9 +48,9 @@ const LogInPage = ({ loginUser, loggedIn }) => {
 
         } catch (err) {
             if (err.message === '401') {
-                setErrorMessage('* Wrong password');
+                setErrorMessage('Wrong password');
             } else if (err.message === '404') {
-                setErrorMessage('* No account with that username');
+                setErrorMessage('No account with that username');
             } else {
                 console.log(err.message);
             }
@@ -69,6 +70,10 @@ const LogInPage = ({ loginUser, loggedIn }) => {
                 <p className="sign-up-heading">Anthology</p>
             </div>
             <h2 className="sign-up-sub-heading">To continue, log in to Anthology.</h2>
+            <div className={errorMessage ? 'log-in-error-container' : 'hidden log-in-error-container'}>
+                <img className="log-in-error-icon" src={errorIcon} />
+                <p className="log-in-error-message">{errorMessage}</p>
+            </div>
             <div className="input-container">
                 <p className="input-label">Username</p>
                 <input className="input" placeholder="Username" value={username} onChange={e => setUsername(e.target.value)} required/>
@@ -78,7 +83,6 @@ const LogInPage = ({ loginUser, loggedIn }) => {
                 <input className="input" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} required/>
             </div>
             <button className="sign-up-form-button">Log In</button>
-            <p className={errorMessage ? 'error-message' : 'hidden error-message'}>{errorMessage}</p>
             <hr className="line-break" />
             <p className="login-footer">Don't have an account?</p>
             <NavLink className="signup-redirect-button" to="/signup">SIGN UP FOR ANTHOLOGY</NavLink>
