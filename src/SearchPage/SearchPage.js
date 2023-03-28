1f7220d14113806b2b09b437d703812d9b36bc28
Tomas Salgado <tomassalgadodev@gmail.com>
2023-03-28 11:39:11 -0600

    feat: Write fetchSearchData async function that implements the new artistSearchData get request in the SearchPage component

--- src/SearchPage/SearchPage.js
+++ src/SearchPage/SearchPage.js
@@ -9,16 +9,34 @@ const SearchPage = ({ searchTerm }) => {
     const [loading, setLoading] = useState(true);
     const [searchResults, setSearchResults] = useState({});
 
-    useEffect(() => {
-
+    const fetchSearchData = async () => {
         setLoading(true);
 
-        fetch(`http://localhost:8000/api/v1/artistSearch/${searchTerm}`)
-            .then(res => res.json())
-            .then(data => {
-                setSearchResults(data);
-                setLoading(false);
-            });
+        try {
+            const response = await fetch(`http://localhost:8000/api/v1/artistSearchData/${searchTerm}`);
+
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+
+            const data = await response.json();
+
+            setSearchResults(data.data.searchV2.artists.items);
+            setLoading(false);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    useEffect(() => {
+        setLoading(true);
+        fetchSearchData();
+        // fetch(`http://localhost:8000/api/v1/artistSearch/${searchTerm}`)
+        //     .then(res => res.json())
+        //     .then(data => {
+        //         setSearchResults(data);
+        //         setLoading(false);
+        //     });
             
     }, [searchTerm]);
 
