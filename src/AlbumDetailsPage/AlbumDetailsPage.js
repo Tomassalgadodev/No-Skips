bf9acd102a797e91ebdd0d29c835f79427b39b55
Tomas Salgado <tomassalgadodev@gmail.com>
2023-03-29 11:00:07 -0600

    feat: AlbumDetailsPage component fetchs album with the proper id based of the url param

--- src/AlbumDetailsPage/AlbumDetailsPage.js
+++ src/AlbumDetailsPage/AlbumDetailsPage.js
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './AlbumDetailsPage.css';
 
-const AlbumDetailsPage = () => {
+const AlbumDetailsPage = ({ albumID, likedAlbums, saveAlbum, removeAlbum }) => {
+
+    const [albumData, setAlbumData] = useState({});
+    const [loading, setLoading] = useState(true);
+
+    const fetchAlbumData = async () => {
+        try {
+            const response = await fetch(`http://localhost:8000/api/v1/album/${albumID}`);
+
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+
+            const data = await response.json();
+
+            console.log(data)
+
+            setAlbumData(data);
+            setLoading(false);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    useEffect(() => {   
+        fetchAlbumData();
+    }, [])
+
+
     return (
-        <h1>Album Details Page</h1>
+        <React.Fragment>
+            {loading && <h1>-- Loading --</h1>}
+            {!loading && <h1>Album Details Page</h1>}
+        </React.Fragment>
     )
 }
 
