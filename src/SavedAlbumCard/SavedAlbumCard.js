b3bb26733db94f26cba584d8bfd82d249bed9c05
Tomas Salgado <tomassalgadodev@gmail.com>
2023-04-07 13:04:22 -0600

    feat: Make saved song cards display dropdown when clicking heart icon instead of removing the album

--- src/SavedAlbumCard/SavedAlbumCard.js
+++ src/SavedAlbumCard/SavedAlbumCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import './SavedAlbumCard.css';
 
@@ -8,26 +8,75 @@ const SavedAlbumCard = ({ link, albumArt, albumTitle, yearReleased, artistName, 
 
     const history = useHistory();
 
+    const [albumArtTop, setAlbumArtTop] = useState('20px');
+    const [albumTitleTop, setAlbumTitleTop] = useState('295px');
+    const [albumDetailsTop, setAlbumDetailsTop] = useState('335px');
+    const [songModalStyle, setSongModalStyle] = useState({ bottom: '70px', height: '0px', border: 'none' });
+    const [songModalActive, setSongModalActive] = useState(false);
+    const [loading, setLoading] = useState(true);
+    const [startedFetch, setStartedFetch] = useState(false);
+    const [albumData, setAlbumData] = useState({});
+    const [likedSongs, setLikedSongs] = useState([]);
+    const [showDropDown, setShowDropDown] = useState(false);
+
+    const toggleSongModal = () => {
+        if (songModalActive) {
+            setAlbumArtTop('20px');
+            setAlbumTitleTop('295px');
+            setAlbumDetailsTop('335px');
+            setSongModalStyle({ bottom: '70px', height: '0px' });
+            setSongModalActive(false);
+        } else {
+            setAlbumArtTop('-300px');
+            setAlbumTitleTop('20px');
+            setAlbumDetailsTop('410px');
+            setSongModalStyle({ bottom: '20px', height: '320px', border: 'solid 1px #B3B3B3' });
+            setSongModalActive(true);
+        }
+    }
+    
+    const handleHeartIconClick = () => {
+        if (songModalActive) {
+            toggleSongModal();
+        } else {
+            setShowDropDown(!showDropDown);
+        }
+    }
+
     return (
-        <div className="album-card" id={link}>
-            <img 
-                className="album-art" 
-                src={albumArt} 
-                onClick={() => window.open(link, '_blank')}
-            />
-            <div className="title-container">
-                <div className="title-box">
-                    <h2 onClick={() => history.push(`/album/${albumID}`)} className="album-title">{albumTitle}</h2>
-                </div>
+        <div className="album-card-wrapper">
+            <div className="album-card" id={link}>
                 <img 
-                    className="heart-icon"
-                    src={likedIcon} 
-                    onClick={() => removeAlbum({ link })}
+                    className="album-art" 
+                    src={albumArt} 
+                    onClick={() => window.open(link, '_blank')}
                 />
+                <div className="title-container">
+                    <div className="title-box">
+                        <h2 onClick={() => history.push(`/album/${albumID}`)} className="album-title">{albumTitle}</h2>
+                    </div>
+                    <img 
+                        className="heart-icon"
+                        src={likedIcon} 
+                        onClick={handleHeartIconClick}
+                    />
+                </div>
+                <div className="details-container">
+                    <p className="album-details">{yearReleased} • <span className="saved-album-artist-link" onClick={() => history.push(`/artist/${artistID}`)}>{artistName}</span></p>
+                </div>
             </div>
-            <div className="details-container">
-                <p className="album-details">{yearReleased} • <span className="saved-album-artist-link" onClick={() => history.push(`/artist/${artistID}`)}>{artistName}</span></p>
-            </div>
+            {showDropDown &&
+                <div className="remove-edit-menu">
+                    <div 
+                        className="remove-edit-menu-button"
+                        // onClick={showSongs}
+                    >Edit album</div>
+                    <div 
+                        className="remove-edit-menu-button"
+                        // onClick={handleRemoveAlbum}
+                    >Remove album</div>
+                </div>        
+            }
         </div>
     )
 }
